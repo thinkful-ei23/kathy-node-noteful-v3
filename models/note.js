78ad9b735b9ba52465a14afe738d972c4bb4ec0b
kathyleboeuf <kleboeuf@townisp.com>
2018-08-14 18:01:41 -0400

    buried __v + converted _id to id

--- models/note.js
+++ models/note.js
@@ -19,6 +19,14 @@ noteSchema.methods.serialize = function () {
 	};
 };
 
+noteSchema.set('toObject', {
+	virtuals: true,     // include built-in virtual `id`
+	versionKey: false,  // remove `__v` version key
+	transform: (doc, ret) => {
+		delete ret._id; // delete `_id`
+	}
+});
+
 
 
 
