8bd5062dc0cb04fcc32ce04550ad5d944830a815
kathyleboeuf <kleboeuf@townisp.com>
2018-08-16 14:58:34 -0400

    thru update seed data

--- utils/seed-database.js
+++ utils/seed-database.js
@@ -4,16 +4,29 @@ const mongoose = require('mongoose');
 
 const { MONGODB_URI } = require('../config');
 const Note = require('../models/note');
+const Folder = require('../models/folder');
 
-const seedNotes = require('../db/seed/notes');
+const seedNotes = require('../db/seed/notes'); git a
+const seedFolders = require('../db/seed/folders');
+
+
+// mongoose.connect(MONGODB_URI)
+// 	.then(() => mongoose.connection.db.dropDatabase())
+// 	.then(() => Note.insertMany(seedNotes))
+// 	.then(results => {
+// 		console.info(`Inserted ${results.length} Notes`);
+// 	})
+// 	.then(() => mongoose.disconnect())
+// 	.catch(err => {
+// 		console.error(err);
+// 	});
 
 mongoose.connect(MONGODB_URI)
 	.then(() => mongoose.connection.db.dropDatabase())
-	.then(() => Note.insertMany(seedNotes))
-	.then(results => {
-		console.info(`Inserted ${results.length} Notes`);
+	.then(() => {
+		return Promise.all([
+			Note.insertMany(seedNotes),
+			Folder.insertMany(seedFolders),
+			Folder.createIndexes()
+		]);
 	})
-	.then(() => mongoose.disconnect())
-	.catch(err => {
-		console.error(err);
-	});
